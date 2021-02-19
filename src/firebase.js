3e5384f6373690dcb5d764b408b0d8f72a02d3ea
sandesh <sundesz@gmail.com>
2021-02-19 17:49:09 +0200

    Added firebase setting

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDbnkE1YaoSGBPVYirBYEhj_rG8UsHnDvs",
+    authDomain: "todo-app-dd77d.firebaseapp.com",
+    projectId: "todo-app-dd77d",
+    storageBucket: "todo-app-dd77d.appspot.com",
+    messagingSenderId: "456086055504",
+    appId: "1:456086055504:web:8b60e924f6bc7b409e3f55",
+    measurementId: "G-7F4YHTHHPM"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
